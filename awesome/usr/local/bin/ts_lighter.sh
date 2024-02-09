ff122f64ab8f0876617aa0b85584c5ae38986cab
Alexander Sarmanow <asarmanow@gmail.com>
2024-02-09 14:22:12 +0100

    awesome: backlight options between intel and amd gpu
    Signed-off-by: Alexander Sarmanow <asarmanow@gmail.com>

--- awesome/usr/local/bin/ts_lighter.sh
+++ awesome/usr/local/bin/ts_lighter.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-read < "/sys/class/backlight/amdgpu_bl0/brightness" brightness
-var1=20
-brightness=`expr $brightness + $var1`
-echo 250 | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
+
+STEP_SIZE=20
+
+if [ -d /sys/class/backlight/amdgpu_bl0 ]; then
+    read < "/sys/class/backlight/amdgpu_bl0/brightness" brightness
+    var1=$STEP_SIZE
+    brightness=`expr $brightness + $var1`
+    echo 250 | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
+else
+    xbacklight -inc $STEP_SIZE
+fi
