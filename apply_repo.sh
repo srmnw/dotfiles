80fcd037d67a4eddad4c6dace250e1f7f55489b8
Alexander Sarmanow <asarmanow@gmail.com>
2024-02-12 23:37:22 +0100

    script: add install directories (for awesome tools)
    Signed-off-by: Alexander Sarmanow <asarmanow@gmail.com>

--- apply_repo.sh
+++ apply_repo.sh
@@ -1,26 +1,42 @@
 #! /usr/bin/bash
 
-declare -a dotfiles=(".bashrc"
-		  ".bash_aliases"
-		  ".tmux.conf"
-		  ".vimrc"
-		 )
+declare -a dotfiles=(
+    ".bashrc"
+    ".bash_aliases"
+    ".tmux.conf"
+    ".vimrc"
+    ".gitconfig"
+)
 
-declare -a dir=(""
-	       )
+declare -a install_dirs=(
+    "usr"
+)
 
 echo "Applying repo ..."
 
 for f in "${dotfiles[@]}"; do
-	printf "\n"
+    printf "\n"
 
-	while true; do
-		read -n 1 -p ">>>>>> apply $f file to filesystem?[y/n]:" yn
-		case $yn in
-			[Yy]* ) cp $f ~/$f; break;;
-			[Nn]* ) break;;
-			*     ) echo "please answer yes or no";;
-		esac
-	done
+    while true; do
+        read -n 1 -p ">>>>>> apply $f file to filesystem?[y/n]:" yn
+        case $yn in
+            [Yy]* ) cp $f ~/$f; break;;
+            [Nn]* ) break;;
+            *     ) echo "please answer yes or no";;
+        esac
+    done
 done
 
+echo "Installing directories...\n"
+for d in "${install_dirs[@]}"; do
+    printf "\n"
+
+    while true; do
+        read -n 1 -p ">>>>>> apply $f directory to filesystem?[y/n]:" yn
+        case $yn in
+            [Yy]* ) cp -r $f /$f; break;;
+            [Nn]* ) break;;
+            *     ) echo "please answer yes or no";;
+        esac
+    done
+done
